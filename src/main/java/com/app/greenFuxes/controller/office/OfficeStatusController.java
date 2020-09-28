fe0701a1cfc2eb670fb7ce16d5a217509624220a
Divad001 <gyoredavid1993@gmail.com>
2020-09-28 17:02:56 +0200

    fixed set-headcount

--- src/main/java/com/app/greenFuxes/controller/office/OfficeStatusController.java
+++ src/main/java/com/app/greenFuxes/controller/office/OfficeStatusController.java
@@ -2,11 +2,11 @@ package com.app.greenFuxes.controller.office;
 
 import com.app.greenFuxes.dto.office.CapacityDTO;
 import com.app.greenFuxes.service.office.OfficeService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +22,8 @@ public class OfficeStatusController {
   }
 
   @PostMapping("/set-headcount")
-  public ResponseEntity<?> setHeadCount(@RequestBody CapacityDTO capacityDTO) throws Exception {
+  public ResponseEntity<?> setCapacity(@RequestBody CapacityDTO capacityDTO) throws Exception {
     officeService.setHeadCount(capacityDTO.getCapacity());
     return new ResponseEntity<>(HttpStatus.OK);
   }
-
 }
