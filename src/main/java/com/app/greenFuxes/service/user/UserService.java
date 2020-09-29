4a317caf745dcf75f0210e3f3169cad9f40d8017
Viktor94 <viktor2.nagy@gmail.com>
2020-09-29 15:55:12 +0200

    Profile image set up changed

--- src/main/java/com/app/greenFuxes/service/user/UserService.java
+++ src/main/java/com/app/greenFuxes/service/user/UserService.java
@@ -8,26 +8,24 @@ import com.app.greenFuxes.exception.user.UserManipulationException;
 import com.app.greenFuxes.exception.user.UserNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
-    User register(RegistrationDTO registrationDTO) throws UserManipulationException;
+  User register(RegistrationDTO registrationDTO) throws UserManipulationException;
 
-    List<User> getUsers();
+  List<User> getUsers();
 
-    User findByUsername(String username) throws UserNotFoundException;
+  User findByUsername(String username) throws UserNotFoundException;
 
-    User findByEmail(String email) throws EmailNotFoundException;
+  User findByEmail(String email) throws EmailNotFoundException;
 
-    User addUser(NewUserDTO newUserDTO) throws UserManipulationException, IOException;
+  User addUser(NewUserDTO newUserDTO) throws UserManipulationException, IOException;
 
-    User findById(Long id) throws UserNotFoundException;
+  User findById(Long id) throws UserNotFoundException;
 
-    void deleteUser(Long id) throws UserNotFoundException;
+  void deleteUser(Long id) throws UserNotFoundException;
 
-    User updateProfileImg(Long id, MultipartFile profileImg)
-        throws UserManipulationException, IOException;
+  void activateUser(Long id) throws UserNotFoundException;
 
-    void activateUser(Long id) throws UserNotFoundException;
+  String getUserImageUrl(Long id) throws UserNotFoundException;
 }
