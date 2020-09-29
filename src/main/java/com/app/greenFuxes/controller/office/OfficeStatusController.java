3ae2a89dbe3f5308b92ae6e568c805b76109fb58
Divad001 <gyoredavid1993@gmail.com>
2020-09-29 17:31:08 +0200

    added LEDservice and test endpoints:

--- src/main/java/com/app/greenFuxes/controller/office/OfficeStatusController.java
+++ src/main/java/com/app/greenFuxes/controller/office/OfficeStatusController.java
@@ -1,6 +1,8 @@
 package com.app.greenFuxes.controller.office;
 
+import com.app.greenFuxes.dto.LED.DurationDTO;
 import com.app.greenFuxes.dto.office.CapacityDTO;
+import com.app.greenFuxes.service.LED.LEDService;
 import com.app.greenFuxes.service.office.OfficeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,15 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class OfficeStatusController {
 
   private final OfficeService officeService;
+  private final LEDService ledService;
 
   @Autowired
-  public OfficeStatusController(OfficeService officeService) {
+  public OfficeStatusController(OfficeService officeService,
+      LEDService ledService) {
     this.officeService = officeService;
+    this.ledService = ledService;
   }
 
   @PostMapping("/set-headcount")
   public ResponseEntity<?> setCapacity(@RequestBody CapacityDTO capacityDTO) throws Exception {
     officeService.setHeadCount(capacityDTO.getCapacity());
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/led-send/test")
+  public ResponseEntity<?> testLED(@RequestBody DurationDTO durationDTO)
+      throws Exception {
+    ledService.send(durationDTO);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/led-stop/test")
+  public ResponseEntity<?> stopTest() {
+    ledService.stop();
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
