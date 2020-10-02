ab2c67f39c2e7d005917fd1608437cc684d58fbc
szukacs <szukacs.gergely@gmail.com>
2020-10-02 12:33:25 +0200

    ez megy

--- src/main/java/com/app/greenFuxes/controller/office/OfficeController.java
+++ src/main/java/com/app/greenFuxes/controller/office/OfficeController.java
@@ -3,10 +3,13 @@ package com.app.greenFuxes.controller.office;
 import com.app.greenFuxes.dto.DateDTO;
 import com.app.greenFuxes.dto.office.UsersInOfficeDTO;
 import com.app.greenFuxes.entity.office.Office;
+import com.app.greenFuxes.entity.reservedDate.ReservedDate;
 import com.app.greenFuxes.entity.user.User;
 import com.app.greenFuxes.service.canteen.CanteenService;
 import com.app.greenFuxes.service.office.OfficeService;
 import com.app.greenFuxes.service.reserveDate.ReserveDateService;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -44,8 +47,13 @@ public class OfficeController {
 
   @PostMapping("/check-date")
   public ResponseEntity<?> checkDate(@RequestBody DateDTO dateDTO) {
-    List<User> list = reserveDateService.findByDate(dateDTO.getDate()).getUsersInOffice();
+    ReservedDate reservedDate = reserveDateService.findByDate(dateDTO.getDate());
     Office office = officeService.findById(1L);
+    if (reservedDate == null) {
+      UsersInOfficeDTO defaultDTO = new UsersInOfficeDTO(new ArrayList<>(), office.getCapacity(), office.getCapacity());
+      return new ResponseEntity<>(defaultDTO, HttpStatus.OK);
+    }
+    List<User> list = reserveDateService.findByDate(dateDTO.getDate()).getUsersInOffice();
     UsersInOfficeDTO usersInOfficeDTO =
         new UsersInOfficeDTO(list, office.getCapacity() - list.size(), office.getCapacity());
     return new ResponseEntity<>(usersInOfficeDTO, HttpStatus.OK);
